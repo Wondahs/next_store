0b73a53ae8e4f3c412b03557d2ad2b9de23d5a1c
Wonders Victor <wondersprince1@gmail.com>
2024-12-31 19:42:29 +0100

    testing websocket

--- src/chatroom/chatroom.module.ts
+++ src/chatroom/chatroom.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatroomService } from './chatroom.service';
 import { ChatroomController } from './chatroom.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ChatroomGateway } from './chatroom.gateway';
 
 @Module({
   controllers: [ChatroomController],
-  providers: [ChatroomService, PrismaService],
+  providers: [ChatroomService, PrismaService, ChatroomGateway],
 })
 export class ChatroomModule {}

8f4fda6a2ed37f91c8f2572208e648b0932636a8
Wonders Victor <wondersprince1@gmail.com>
2024-12-31 19:52:07 +0100

    Add JWT module configuration to chatroom module for authentication

--- src/chatroom/chatroom.module.ts
+++ src/chatroom/chatroom.module.ts
@@ -1,10 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ChatroomService } from './chatroom.service';
 import { ChatroomController } from './chatroom.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ChatroomGateway } from './chatroom.gateway';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+    imports: [PrismaModule,
+      JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '1d' },
+        }),
+      })
+    ],
   controllers: [ChatroomController],
   providers: [ChatroomService, PrismaService, ChatroomGateway],
 })
